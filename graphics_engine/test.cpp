6923947a22dfc11e1b85da432ad2f104c62adeeb
César Van Leuffelen <info@netbrew.be>
2024-05-20 16:22:50 +0200

    files after stream 1
    - setup of vs code
    - setup for C++ development
    - some testing

---
+++ graphics_engine/test.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+
+
+int main()
+{
+    int age;
+    std::string name;
+
+    std::cout << "Enter your name: " << std::endl;
+    std::cin >> name;
+
+    std::cout << "Enter your age: " << std::endl;
+    std::cin >> age;
+
+    std::cout << "name: " << name << std::endl;
+    std::cout << "age: " << age << std::endl;
+
+}
